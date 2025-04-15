7593880113c4d909d4ceed0d517e06acd404487b
Infernaut <r.balase.546499@umindanao.edu.ph>
2025-04-15 15:14:16 +0800

    update(2)

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/register', [AuthController::class, 'showRegister']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::middleware(['guest'])->group(function () {
+    Route::get('/register', [AuthController::class, 'showRegister']);
+    Route::post('/register', [AuthController::class, 'register']);
 
-
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-
+    Route::get('/login', [AuthController::class, 'showLogin']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
 
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
 
