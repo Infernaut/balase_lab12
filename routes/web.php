08a1ffd3e21f0c5f08ca21ec8102ceab26cbbb7e
Infernaut <r.balase.546499@umindanao.edu.ph>
2025-04-15 15:09:31 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/register', [AuthController::class, 'showRegister']);
 Route::post('/register', [AuthController::class, 'register']);
 
 
-Route::get('/login', [AuthController::class, 'showLogin']);
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 
 
